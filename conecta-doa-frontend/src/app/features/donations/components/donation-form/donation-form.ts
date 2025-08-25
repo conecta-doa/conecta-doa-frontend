8d678cb7850fc7fbcd2140227e3db3cc1453c49d
Domingos Capewa <domingoscapewa08@gmail.com>
2025-08-25 02:46:49 -0300

    feat: add initial structure for dashboard, donations, login, and user features with components and routing

---
+++ conecta-doa-frontend/src/app/features/donations/components/donation-form/donation-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donation-form',
+  imports: [],
+  templateUrl: './donation-form.html',
+  styleUrl: './donation-form.css'
+})
+export class DonationForm {
+
+}

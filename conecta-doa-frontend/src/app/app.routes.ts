6483c59b4080bf6eb7449d3dbce36d27473975f0
Domingos Capewa <domingoscapewa08@gmail.com>
2025-09-01 03:03:38 -0300

    feat: add basic structure for Home, Dashboard, Register, Card, Footer, and Navbar components

--- conecta-doa-frontend/src/app/app.routes.ts
+++ conecta-doa-frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  // { path: 'about', component: AboutComponent },
+  // { path: '**', component: NotFoundComponent }
+];

d96bde91416cdb96c0aabbdb3a766f7a2134f0e0
V1ventura <venturavinicius6@gmail.com>
2025-09-07 01:17:36 -0300

    Páginas do painel do doador e instituições concluídas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
+import { Paineldoador } from './components/instituition/paineldoador/paineldoador';
+import { InstituicaoComponent } from './components/instituition/instituicoes/instituicao';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: Home },
+  { path: 'painel', component: Paineldoador },
+  { path: 'instituicao', component: InstituicaoComponent },
+
   // { path: 'about', component: AboutComponent },
   // { path: '**', component: NotFoundComponent }
 ];

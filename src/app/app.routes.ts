ebc1b957f2c914c2a56c86006b027e05ace1f49d
Domingos Capewa <domingoscapewa08@gmail.com>
2025-09-11 21:00:46 -0300

    Add donor dashboard layout and styles; include header and footer components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,7 @@ import { Login } from './shared/components/login/login';
 
 import { RegisterComponent } from './components/instituition/register/register';
 import { Register as RegisterDoador } from './components/donor/register/register';
-// import { Dashboard as DonorDasbboard } from './components/donor/dashboard/dashboard';
-
-
+import { Dashboard as DonorDasbboard } from './components/donor/dashboard/dashboard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -15,13 +13,9 @@ export const routes: Routes = [
 
   { path: 'instituition/register', component: RegisterComponent },
 
-
   { path: 'donor/register', component: RegisterDoador },
-  // { path: 'donor/dashboard', component: DonorDasbboard },
+  { path: 'donor/dashboard', component: DonorDasbboard },
 
-
- // { path: 'about', component: SobreNosComponent },
+  // { path: 'about', component: SobreNosComponent },
   // { path: '**', component: NotFoundComponent }
-
 ];
-
--- src/app/components/donor/dashboard/dashboard.ts
+++ src/app/components/donor/dashboard/dashboard.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../../shared/components/footer/footer";
+import { HeaderComponent } from "../../../shared/components/header/header";
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css'
+    selector: 'app-dashboard',
+    standalone: true,
+    templateUrl: './dashboard.html',
+    styleUrl: './dashboard.css',
+    imports: [FooterComponent, HeaderComponent]
 })
 export class Dashboard {
 

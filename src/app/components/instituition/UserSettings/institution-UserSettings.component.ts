bfa4460d146a75f534a97cec13cafa9da0accc38
Lucas Moreira <moreira.nlucas51@gmail.com>
2025-10-30 22:19:43 -0300

    feat(user-config): Implementa tela e rotas de configuração de usuário

---
+++ src/app/components/instituition/UserSettings/institution-UserSettings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-settings', 
+  templateUrl: './institution-UserSettings.component.html',
+  styleUrls: ['./institution-UserSettings.component.css'] 
+})
+export class UserSetings  {
+
+}
